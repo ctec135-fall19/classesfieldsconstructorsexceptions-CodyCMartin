c2ee2e19ef72d915daf85790b0689e1b2f6147c0
codycmartin <codycmartin89@gmail.com>
2019-10-10 16:53:41 -0700

    Thrusday Labs

---
+++ Labs/Classes/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Employee
+    {
+        // fields
+​
+        // constructors
+​
+        // methods
+        public void Hello()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Labs/Classes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate object
+            Employee employee = new Employee();
+​
+            // method call
+            employee.Hello();
+        }
+    }
+}
